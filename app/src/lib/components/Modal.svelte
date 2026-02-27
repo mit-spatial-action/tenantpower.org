8a9526d2674e8210975d2d672924876f634e54e5
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2026-02-27 16:36:10 -0500

    #3 implement modal

---
+++ app/src/lib/components/Modal.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { fade, fly } from 'svelte/transition';
+    let { title = null, children } = $props();
+    let active = $state(true);
+    const onclick = () => active = !active;
+</script>
+
+{#if active}
+<div class="modal is-active" transition:fade={{ duration: 200 }}>
+    <div class="modal-background"></div>
+    <div class="modal-card" transition:fly={{ y: 600, duration: 800 }}>
+        {#if title}
+        <header class="modal-card-head has-background-primary">
+            <h1 class="modal-card-title title is-3 has-text-white">{ title }</h1>
+        </header>
+        {/if}
+        <section class="modal-card-body">
+            {@render children?.()}
+        </section>
+        <footer class="modal-card-foot">
+            <button class="button is-primary is-fullwidth" {onclick}>Continue→</button>
+        </footer>
+    </div>
+</div>
+{/if}
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,4 +1,35 @@
 <script>
-    import Map from "$lib/Map.svelte";
+    import Map from "$lib/components/Map.svelte";
+    import Modal from "$lib/components/Modal.svelte";
 </script>
+
+<Modal title="Tenant Power">
+    <div class="block">
+        <article class="message is-primary">
+            <div class="message-header">
+                <p>🚨 Warning! 🚨</p>
+            </div>
+            <div class="message-body">
+                The Tenant Power database has not been updated since 2021. We are continuing to make the tool available for archival purposes, but information gleaned from this platform should be treated with <strong>extreme</strong> caution.
+            </div>
+        </article>
+    </div>
+    <div class="block">
+        Tenant Power used city assessor's data from 
+        <a href="https://www.cityofboston.gov/assessing/search/">Boston</a>, 
+        <a href="https://www.cambridgema.gov/PropertyDatabase">Cambridge</a>, 
+        <a href="http://gis.vgsi.com/SomervilleMA/Search.aspx">Somerville</a>, 
+        <a href="http://gis.vgsi.com/medfordma/">Medford</a>, and
+        <a href="http://apps.brooklinema.gov/assessors/propertylookup.asp">Brookline</a>, Massachusetts
+        to identify additional properties that are likely owned by your landlord. We're able to make pretty 
+        good guesses! But this data can only tell us so much---we encourage you to check 
+        <a href="http://www.masslandrecords.com/">Massachusetts Land Records</a>
+        and/or the assessors databases linked above to confirm ownership.
+    </div>
+    <div class="block">
+        Tenant Power was a collaboration between the
+        <a href="https://mutualaidmamas.com/">Mutual Aid Medford and Somerville (MAMAS)</a> 
+        Housing Justice Working Group and <a href="">Eric Robsky Huntley</a> of the <a href="">MIT Spatial Action & Analysis Research Group</a>.
+    </div>
+</Modal>
 <Map/>
