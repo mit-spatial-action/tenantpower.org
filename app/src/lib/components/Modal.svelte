26be4da361234c2282b21a4bb8f5b557746f0686
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2026-03-02 09:50:25 -0500

    #3 generalize modal

--- app/src/lib/components/Modal.svelte
+++ app/src/lib/components/Modal.svelte
@@ -1,29 +1,12 @@
 <script lang="ts">
-    import { fade, fly } from "svelte/transition";
-    let { title = null, children } = $props();
-    let active = $state(true);
-    const onclick = () => (active = !active);
+    let { active = $bindable(false), children } = $props();
+    import { fade } from "svelte/transition";
+    const close = () => (active = false);
 </script>
 
 {#if active}
     <div class="modal is-active" transition:fade={{ duration: 200 }}>
-        <div class="modal-background"></div>
-        <div class="modal-card" transition:fly={{ y: 600, duration: 800 }}>
-            {#if title}
-                <header class="modal-card-head has-background-primary">
-                    <h1 class="modal-card-title title is-3 has-text-white">
-                        {title}
-                    </h1>
-                </header>
-            {/if}
-            <section class="modal-card-body">
-                {@render children?.()}
-            </section>
-            <footer class="modal-card-foot">
-                <button class="button is-primary is-fullwidth" {onclick}
-                    >Continue→</button
-                >
-            </footer>
-        </div>
+        <div class="modal-background" onclick={close}></div>
+        {@render children?.(close)}
     </div>
 {/if}
